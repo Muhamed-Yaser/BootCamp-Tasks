d857f3d8a76580e8b3da2127d9b47c7b03505867
Muhamed <muhamed.yasser.muhammed@gmail.com>
2026-08-06 01:54:20 +0300

    Task1

---
+++ Task1/src/com/example/Shapes/Circle.java
@@ -0,0 +1,10 @@
+package com.example.Shapes;
+
+import com.example.Shape;
+
+public class Circle implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("Draw circle Shape");
+    }
+}
---
+++ Task1/src/com/example/Shapes/Rectangle.java
@@ -0,0 +1,10 @@
+package com.example.Shapes;
+
+import com.example.Shape;
+
+public class Rectangle implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("Draw Rectangle Shape");
+    }
+}
