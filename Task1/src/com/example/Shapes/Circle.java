43b4cea89fa675736ffd368b4c6bd5de3e5a47ff
Muhamed <muhamed.yasser.muhammed@gmail.com>
2026-08-06 02:05:19 +0300

    #Task1

--- Task1/src/com/example/Shapes/Circle.java
+++ Task1/src/com/example/Shapes/Circle.java
@@ -3,6 +3,7 @@ package com.example.Shapes;
 import com.example.Shape;
 
 public class Circle implements Shape {
+    //print
     @Override
     public void draw() {
         System.out.println("Draw circle Shape");
